4f8a5e7bad90587fccf62b58263b0323de2345b4
Mariana Cousseiro de Lima <limatainer@gmail.com>
2024-10-14 13:33:13 +0100

    search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -25,7 +25,8 @@ export default function Search() {
             </p>
             <Link
               to="/submissions"
-              className="inline-block py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white rounded-lg font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-900"
+              className="inline-block py-2 px-4 bg-blue-600 hover:bg-blue-700
+               text-white rounded-lg font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-900"
             >
               Back
             </Link>
